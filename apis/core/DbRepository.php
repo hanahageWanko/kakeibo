99ac28a09cf0c14e00981d656c59edc1058a7de6
nakagawa <237s10mimi@gmail.com>
2021-02-13 00:26:14 +0900

    apis/core/DbRepository.php

---
+++ apis/core/DbRepository.php
@@ -0,0 +1,33 @@
+<?php
+  abstract class DbRepository
+  {
+      protected $con;
+
+      public function __construct($con)
+      {
+          $this->setConnection($con);
+      }
+
+      public function setConnection($con)
+      {
+          $this->con = $con;
+      }
+
+      public function execute($sql, $params = [])
+      {
+          $stmt = $this->con->prepare($sql);
+          $stmt->execute($params);
+
+          return $stmt;
+      }
+
+      public function fetch($sql, $params = [])
+      {
+          return $this->execute($sql, $params)->getch(PDO::FETCH_ASSOC);
+      }
+
+      public function fetchAll($sql, $params = [])
+      {
+          return $this->execute($sql, $params)->fetchAll(PDO::FETCH_ASSOC);
+      }
+  }
