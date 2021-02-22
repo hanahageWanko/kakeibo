94bb4ea721aec03119b56ee7a827353ed3b67f90
nakagawa <237s10mimi@gmail.com>
2021-02-23 00:42:03 +0900

    fix view method

--- apis/core/View.php
+++ apis/core/View.php
@@ -1,10 +1,17 @@
 <?php
 class View
 {
-    public static function make($view)
+    public static function make($view, $model, $controller)
     {
         if (Route::isRouteValid()) {
-            require_once(__DIR__ . "/../controllers/$view.php");
+            if(!$model) {
+              $model = $view;
+            }
+            if(!$controller) {
+              $controller = $view;
+            }
+            require_once(__DIR__ . "/../controllers/$controller.php");
+            require_once(__DIR__ . "/../models/$model.php");
             require_once(__DIR__ . "/../views/$view.php");
             return 1;
         }
