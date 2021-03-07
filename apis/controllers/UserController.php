32ddc35ae2acc87a2ed812ba603dd0294e8cdf7c
nakagawa <237s10mimi@gmail.com>
2021-03-07 12:00:00 +0900

    remove: extra code from userController

--- apis/controllers/UserController.php
+++ apis/controllers/UserController.php
@@ -12,7 +12,6 @@ class UserController extends Controller
     }
 
     public function insertAction() {
-      $this->getContents();
       // TODO: フロント実実装時にコメントをとる
       // $token = $this->request->getPost('_token');
       // if (!$this->checkCsrfToken('user/insert', $token)) {
