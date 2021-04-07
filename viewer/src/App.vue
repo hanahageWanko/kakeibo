07a9f10e80f9bd953b79080d585c546dd195cc76
nakagawa <237s10mimi@gmail.com>
2021-04-08 00:30:52 +0900

    feat: initial axios

--- viewer/src/App.vue
+++ viewer/src/App.vue
@@ -1,16 +1,51 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div>
+    <Navigation />
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <Read :msg="message" />
+    {{ data }}
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import { defineComponent, onMounted, reactive } from "vue";
+import { BaseRepository } from "./axios/Api";
+import Read from "./components/Read.vue";
+import Navigation from "./components/Navigation.vue";
 
 export default defineComponent({
   name: "App",
   components: {
-    HelloWorld,
+    Read,
+    Navigation,
+  },
+
+  setup() {
+    const data: any = reactive({
+      userData: [],
+    });
+    // グローバル変数 axios の代わりに先述の設定の色々追加された AxiosInstance を BaseRepository 経由で使用する
+    async function getUserInfo() {
+      try {
+        await BaseRepository.get("/user/read?id=5").then((res) => {
+          data.userData = res.data;
+          console.log(data.userData);
+        });
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    onMounted(() => {
+      getUserInfo();
+      console.log(data);
+    });
+
+    const message = "Welcome to Your Vue.js + TypeScript App";
+    return {
+      message,
+      data,
+    };
   },
 });
 </script>
