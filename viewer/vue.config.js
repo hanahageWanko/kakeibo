7318ccc5c7eb06f239d2e88180c584e335905e54
nakagawa <237s10mimi@gmail.com>
2021-04-22 00:56:23 +0900

    feat: vue.config.js

---
+++ viewer/vue.config.js
@@ -0,0 +1,12 @@
+var path = require('path')
+​
+module.exports = {
+  configureWebpack: {
+    resolve: {
+      alias: {
+        vue$: 'vue/dist/vue.esm.js',
+        '@': path.resolve(__dirname, 'src/'),
+      },
+    },
+  },
+}
