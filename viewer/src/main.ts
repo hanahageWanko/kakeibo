836c5ed050c47c0e09bde1443e4605e9dc8c4c66
nakagawa <237s10mimi@gmail.com>
2021-05-15 15:33:32 +0900

    feat: add font-awesome

--- viewer/src/main.ts
+++ viewer/src/main.ts
@@ -2,5 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import {key, store} from './store/store'
+import { FontAwesomeIcon } from "@/plugins/font-awesome";
 
-createApp(App).use(router).use(store, key).mount('#app')
+createApp(App)
+.use(router)
+.use(store, key)
+.component("fa", FontAwesomeIcon)
+.mount('#app')
